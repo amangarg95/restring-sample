056b36961ae8707914f151a0b151ddd3b851a8cb
Aman Garg <aman.g@kiprosh.com>
2021-08-12 04:03:51 +0530

    UI Modified

--- app/src/main/java/com/amangarg/localizationdemo/MainActivity.kt
+++ app/src/main/java/com/amangarg/localizationdemo/MainActivity.kt
@@ -1,32 +1,39 @@
 package com.amangarg.localizationdemo
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import com.fasterxml.jackson.databind.ObjectMapper
 import dev.b3nedikt.restring.Restring
-import dev.b3nedikt.reword.Reword
 import dev.b3nedikt.reword.Reword.reword
+import java.io.IOException
+import java.nio.charset.StandardCharsets
 import java.util.*
 import kotlin.collections.HashMap
 
 class MainActivity : BaseActivity() {
-    lateinit var btnEnglish: AppCompatTextView
-    lateinit var btnSpanish: AppCompatTextView
+    lateinit var btnEnglish: AppCompatButton
+    lateinit var btnSpanish: AppCompatButton
     lateinit var englishLocale: Locale
     lateinit var spanishLocale: Locale
     var englishStringsMap: HashMap<String, String> = HashMap()
     var spanishStringsMap: HashMap<String, String> = HashMap()
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnEnglish = findViewById(R.id.tv_english)
-        btnSpanish = findViewById(R.id.tv_spanish)
-        englishStringsMap["text_greeting"] = "Have a nice day!"
-        spanishStringsMap["text_greeting"] = "Que tenga un lindo día!"
+        btnEnglish = findViewById(R.id.btn_english)
+        btnSpanish = findViewById(R.id.btn_spanish)
+
         configureLocales()
+        updateStringsHashmap()
+
         btnEnglish.setOnClickListener {
             updateAppLanguage("en")
         }
@@ -39,6 +46,17 @@ class MainActivity : BaseActivity() {
     private fun configureLocales() {
         englishLocale = Locale("en")
         spanishLocale = Locale("es")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun updateStringsHashmap() {
+        getLocalStringJsonHashmap().forEach {
+            if (it.key == "en_text_greeting") {
+                englishStringsMap["text_greeting"] = it.value
+            } else {
+                spanishStringsMap["text_greeting"] = it.value
+            }
+        }
     }
 
     private fun updateAppLanguage(language: String) {
@@ -55,5 +73,25 @@ class MainActivity : BaseActivity() {
     private fun updateView() {
         val rootView: View = window.decorView.findViewById(android.R.id.content)
         reword(rootView)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun getLocalStringJsonHashmap(): HashMap<String, String> {
+        val listTypeJson: HashMap<String, String> = HashMap()
+        try {
+            applicationContext.assets.open("localized_strings.json").use { inputStream ->
+                val size: Int = inputStream.available()
+                val buffer = ByteArray(size)
+                inputStream.read(buffer)
+                val jsonString = String(buffer, StandardCharsets.UTF_8)
+                JsonHelper().getFlattenedHashmapFromJsonForLocalization(
+                    "",
+                    ObjectMapper().readTree(jsonString),
+                    listTypeJson
+                )
+            }
+        } catch (exception: IOException) {
+        }
+        return listTypeJson
     }
 }
